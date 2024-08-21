2c2ad1a6eeef5ca55791577bd148d71d6542412a
gandharv.jaggi <gandharv.jaggi@collegedunia.com>
2024-08-21 17:10:30 +0530

    country design

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -25,34 +25,37 @@ const Country = ({ data }) => {
 
   return (
     <div className='flex flex-col lg:flex-row'>
-      <div className='grid grid-cols-2 gap-2 flex-1 order-2'>
+      <div className='flex-1 order-2'>
         <Field label='Common Name' value={name.common} />
         <Field label='Official Name' value={name.official} />
-        <Field label='Area' value={`${area} km^2`} />
+        <Field label='Area' value={area && `${area} km^2`} />
         <Field
           label='Currency'
-          value={Object.entries(currencies)
-            .map(([key, value]) => `${value.symbol} ${value.name} (${key})`)
-            ?.join(', ')}
+          value={
+            currencies &&
+            Object.entries(currencies)
+              .map(([key, value]) => `${value.symbol} ${value.name} (${key})`)
+              ?.join(', ')
+          }
         />
         <Field label='Landlocked' value={landlocked ? 'Yes' : 'No'} />
         <Field label='Languages' value={Object.values(languages)?.join(', ')} />
         <Field label='Population' value={population} />
         <Field label='UN Member' value={unMember ? 'Yes' : 'No'} />
       </div>
-      <div className='order-1 lg:order-2'>
-        <img
-          src={flags.png}
-          alt={flags.alt}
-          width={400}
-          className='border mb-4'
-        />
-        <div className='grid grid-cols-2 gap-2'>
-          <Field label='Capital' value={capital?.join(', ')} />
-          <Field label='Continent' value={continents?.join(', ')} />
-          <Field label='Region' value={region} />
-          <Field label='Sub Region' value={subregion} />
-        </div>
+      <div className='shrink-1 order-1 lg:order-2'>
+        {flags.png && (
+          <img
+            src={flags.png}
+            alt={flags.alt}
+            width={400}
+            className='border mb-4'
+          />
+        )}
+        <Field label='Capital' value={capital?.join(', ')} />
+        <Field label='Continent' value={continents?.join(', ')} />
+        <Field label='Region' value={region} />
+        <Field label='Sub Region' value={subregion} />
         {coatOfArms.png && (
           <>
             <div className='text-gray-400 text-sm mb-2'>Coat of Arms</div>

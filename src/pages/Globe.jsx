c297927d4fe1ca5b0571a98a47bd0cfd67932cf8
gandharv.jaggi <gandharv.jaggi@collegedunia.com>
2024-08-21 15:37:13 +0530

    netlify

--- src/pages/Globe.jsx
+++ src/pages/Globe.jsx
@@ -1,6 +1,5 @@
 import * as d3 from 'd3';
 import { useEffect } from 'react';
-import data from '../constants/world.json';
 import { useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
@@ -21,67 +20,69 @@ const Globe = () => {
     const path = d3.geoPath(projection);
     const initialScale = projection.scale();
 
-    const svg = d3
-      .select('#globe')
-      .attr('width', dpr * width)
-      .attr('height', dpr * height)
-      .style('width', `${width}px`);
+    import('../constants/world.json').then((data) => {
+      const svg = d3
+        .select('#globe')
+        .attr('width', dpr * width)
+        .attr('height', dpr * height)
+        .style('width', `${width}px`);
 
-    svg.selectAll('circle').remove();
+      svg.selectAll('circle').remove();
 
-    const globe = svg
-      .append('circle')
-      .attr('id', 'map')
-      .attr('class', 'fill-white stroke-black dark:fill-slate-900')
-      .attr('stroke-width', 1)
-      .attr('cx', width / 2)
-      .attr('cy', height / 2)
-      .attr('r', projection.scale())
-      .lower();
+      const globe = svg
+        .append('circle')
+        .attr('id', 'map')
+        .attr('class', 'fill-white stroke-black dark:fill-slate-900')
+        .attr('stroke-width', 1)
+        .attr('cx', width / 2)
+        .attr('cy', height / 2)
+        .attr('r', projection.scale())
+        .lower();
 
-    svg
-      .selectAll('path')
-      .data(data.features)
-      .join('path')
-      .attr('d', path)
-      .attr('class', countryClasses)
-      .attr('data-country', (d) => d.id)
-      .on('mouseenter', (e) => {
-        e.target.classList = 'fill-red-500 dark:fill-red-900';
-      })
-      .on('mouseleave', (e) => {
-        e.target.classList = countryClasses;
-      })
-      .on('click', (event) => {
-        event.preventDefault();
-        navigate(`/?country=${event.target.dataset.country}`);
-      });
-
-    svg
-      .call(
-        d3.drag().on('drag', (event) => {
-          const rotate = projection.rotate();
-          const k = sensitivity / projection.scale();
-          projection.rotate([
-            rotate[0] + event.dx * k,
-            rotate[1] - event.dy * k
-          ]);
-          const newPath = d3.geoPath().projection(projection);
-          svg.selectAll('path').attr('d', newPath);
+      svg
+        .selectAll('path')
+        .data(data.features)
+        .join('path')
+        .attr('d', path)
+        .attr('class', countryClasses)
+        .attr('data-country', (d) => d.id)
+        .on('mouseenter', (e) => {
+          e.target.classList = 'fill-red-500 dark:fill-red-900';
         })
-      )
-      .call(
-        d3.zoom().on('zoom', (event) => {
-          if (event.transform.k > 0.3) {
-            projection.scale(initialScale * event.transform.k);
+        .on('mouseleave', (e) => {
+          e.target.classList = countryClasses;
+        })
+        .on('click', (event) => {
+          event.preventDefault();
+          navigate(`/?country=${event.target.dataset.country}`);
+        });
+
+      svg
+        .call(
+          d3.drag().on('drag', (event) => {
+            const rotate = projection.rotate();
+            const k = sensitivity / projection.scale();
+            projection.rotate([
+              rotate[0] + event.dx * k,
+              rotate[1] - event.dy * k
+            ]);
             const newPath = d3.geoPath().projection(projection);
             svg.selectAll('path').attr('d', newPath);
-            globe.attr('r', projection.scale());
-          } else {
-            event.transform.k = 0.3;
-          }
-        })
-      );
+          })
+        )
+        .call(
+          d3.zoom().on('zoom', (event) => {
+            if (event.transform.k > 0.3) {
+              projection.scale(initialScale * event.transform.k);
+              const newPath = d3.geoPath().projection(projection);
+              svg.selectAll('path').attr('d', newPath);
+              globe.attr('r', projection.scale());
+            } else {
+              event.transform.k = 0.3;
+            }
+          })
+        );
+    });
   }, [navigate]);
 
   return (
